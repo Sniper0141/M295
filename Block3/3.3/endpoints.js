324d054f7f732fc89cd2594bbeac8ef522d1fd09
Said Simokovic <said.simokovic@digitecgalaxus.ch>
2024-05-14 13:51:21 +0200

    make 3.3 better

--- Block3/3.3/endpoints.js
+++ Block3/3.3/endpoints.js
@@ -1,7 +1,10 @@
+import fs from 'fs'
 import express from 'express';
 
 const port = 3000;
 const app = express();
+
+// Endpoints
 
 app.get("/now", (request, response) => {
     const today = new Date();
@@ -13,51 +16,16 @@ app.get("/zli", (request, response) => {
 });
 
 app.get("/name", (request, response) => {
+    const namesListJson = JSON.parse(fs.readFileSync("namesList.json", "utf8"));
     const randomNumber = Math.floor(Math.random() * 10);
-
-    let name;
-    switch(randomNumber){
-        case 0:
-            name = "Živko Dušan";
-            break;
-        case 1:
-            name = "Biljana Filip";
-            break;
-        case 2:
-            name = "Nikolina Vukašin";
-            break;
-        case 3:
-            name = "Filip Đorđe";
-            break;
-        case 4:
-            name = "Agata Anja";
-            break;
-        case 5:
-            name = "Plamen Vojislava";
-            break;
-        case 6:
-            name = "Ljiljana Tomislav";
-            break;
-        case 7:
-            name = "Bojana Mirka";
-            break;
-        case 8:
-            name = "Isaija Viktorija";
-            break;
-        case 9:
-            name = "Spiridon Slavomir";
-            break;
-        default:
-            name = "bez imena";
-    }
-
-    response.send(name);
+    response.send(namesListJson[randomNumber]);
 });
 
 app.get("/html", (request, response) => {
-    
     response.sendFile("C:/Users/saids/OneDrive - bbw.ch/Desktop/Schule/ÜKs/Files/ZLI_ÜK_M295/M295/Block3/3.3/haha.html");
 });
+
+// Listen on Port 3000
 
 app.listen(port, ()=>{
     console.log(`Endpoints app listening on port ${port}`)
