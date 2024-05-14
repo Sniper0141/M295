a7afc87a616df616f5b7e81ef7bcf74c4dea519d
Said Simokovic <said.simokovic@digitecgalaxus.ch>
2024-05-14 15:43:23 +0200

    better comments

--- Block3/3.3/endpoints.js
+++ Block3/3.3/endpoints.js
@@ -4,17 +4,22 @@ import express from 'express';
 const port = 3000;
 const app = express();
 
+/*
 // Endpoints
+*/
 
+// now
 app.get("/now", (request, response) => {
     const today = new Date();
     response.send(today.getTime().toLocaleString("de-CH", { timeZone: request.params.tz }));
 });
 
+// zli
 app.get("/zli", (request, response) => {
     response.redirect("https://www.zli.ch/");
 });
 
+// name
 app.get("/name", (request, response) => {
     const namesListJson = JSON.parse(fs.readFileSync("namesList.json", "utf8"));
     const randomNumber = Math.floor(Math.random() * namesListJson.length);
@@ -36,11 +41,14 @@ app.delete("/names", (request, response) => {
     console.log('Name "' + request.query.name + '" has been removed.');
 });
 
+// html
 app.get("/html", (request, response) => {
     response.sendFile("C:/Users/saids/OneDrive - bbw.ch/Desktop/Schule/ÜKs/Files/ZLI_ÜK_M295/M295/Block3/3.3/haha.html");
 });
 
+/*
 // Listen on port 3000
+*/
 
 app.listen(port, ()=>{
     console.log(`Endpoints app listening on port ${port}`)
