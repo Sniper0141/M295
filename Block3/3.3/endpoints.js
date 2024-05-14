0c8ae507c7eaaf40bfa115cc7b63e48871eb67ff
Said Simokovic <said.simokovic@digitecgalaxus.ch>
2024-05-14 09:23:56 +0200

    3.3 /zli /name

--- Block3/3.3/endpoints.js
+++ Block3/3.3/endpoints.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 const port = 3000;
 const app = express();
+const router = express.Router()
 
 app.get("/now", (request, response) => {
     const today = new Date();
@@ -9,8 +10,49 @@ app.get("/now", (request, response) => {
 });
 
 app.get("/zli", (request, response) => {
-    const today = new Date();
-    console.log(today)
+    response.redirect("https://www.zli.ch/");
+});
+
+app.get("/name", (request, response) => {
+    const randomNumber = Math.floor(Math.random() * 10);
+
+    let name;
+    switch(randomNumber){
+        case 0:
+            name = "Živko Dušan";
+            break;
+        case 1:
+            name = "Biljana Filip";
+            break;
+        case 2:
+            name = "Nikolina Vukašin";
+            break;
+        case 3:
+            name = "Filip Đorđe";
+            break;
+        case 4:
+            name = "Agata Anja";
+            break;
+        case 5:
+            name = "Plamen Vojislava";
+            break;
+        case 6:
+            name = "Ljiljana Tomislav";
+            break;
+        case 7:
+            name = "Bojana Mirka";
+            break;
+        case 8:
+            name = "Isaija Viktorija";
+            break;
+        case 9:
+            name = "Spiridon Slavomir";
+            break;
+        default:
+            name = "bez imena";
+    }
+
+    response.send(name);
 });
 
 app.listen(port, ()=>{
