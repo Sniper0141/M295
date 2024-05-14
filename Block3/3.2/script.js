c8cc79953ea851956361fa0e876bcdfa5f2b8815
Said Simokovic <said.simokovic@digitecgalaxus.ch>
2024-05-14 09:48:11 +0200

    3.2 very small cleanup

--- Block3/3.2/script.js
+++ Block3/3.2/script.js
@@ -6,10 +6,10 @@ let url ="https://app-prod-ws.meteoswiss-app.ch/v1/plzDetail?plz=";
 
 app.get("/:zip", async (browserRequest,response) => {
     const zip = browserRequest.params.zip;
-    const weatherData = await fetch(url+zip+"00")
-    const data = await weatherData.json()
-        console.log(data);
-        response.send(data);})
+    const weatherData = await fetch(url+zip+"00");
+    const data = await weatherData.json();
+    console.log(data);
+    response.send(data);})
 
 app.listen(port, ()=>{
     console.log(`Example app listening on port ${port}`)
